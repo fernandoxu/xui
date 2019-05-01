b90385866b1c2ae752a1bd887daa1683a2d04bc0
root <root@XLS.localdomain>
2019-05-02 00:55:09 +0800

    config: 搞配置

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    xui: './lib/index.tsx'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'XUI',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, //  ?表示可有可无，$表示结尾
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+}
