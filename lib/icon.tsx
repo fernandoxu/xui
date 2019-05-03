9a1d7c900167ccc75887f2c7bb94fd2f91edd4d3
fernandoxu <fernandoxu9@gmail.com>
2019-05-04 00:05:50 +0800

    feat: 给icon添加onClick事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -5,11 +5,12 @@ import './icon.scss'
 
 interface IconProps {
   name: string
+  onClick: () => void
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   return (
-    <svg className="xui-icon">
+    <svg className="xui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`} />
     </svg>
   )
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,4 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Icon from './icon'
 
-ReactDOM.render(<Icon name="webpack" />, document.getElementById('root'))
+const fn = () => {
+  console.log('fn')
+}
+
+ReactDOM.render(
+  <Icon onClick={fn} name="webpack" />,
+  document.getElementById('root')
+)
