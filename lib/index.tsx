18e9d9ee30bcd9a1fd12cf1d31c42f9819d26851
fernandoxu <fernandoxu9@gmail.com>
2019-05-04 19:13:15 +0800

    feat: 导出组件，发布npm

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,13 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Icon from './icon/icon'
-
-const fn: React.MouseEventHandler = e => {
-  e.preventDefault()
-  console.log(e.target)
-}
-
-ReactDOM.render(
-  <Icon onClick={fn} name="webpack" className="icon-webpack" />,
-  document.getElementById('root')
-)
+export { default as Icon } from './icon/icon'
